e19c15e8201f0a7e1a46b03613ff1503ddf5dcc4
sully <msully725@gmail.com>
2021-01-26 21:25:53 -0500

    Added fetching on an interval. Need to add enable toggle. Fetch interval slider would be nice.

--- aldl-app/src/App.js
+++ aldl-app/src/App.js
@@ -1,19 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+
+const latestLogFetchIntervalMs = 125;
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => savedCallback.current();
+
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+
+      return () => clearInterval(id);
+    }
+  }, [callback, delay]);
+}
 
 function App() {
   const [state, setState] = useState([]);
 
-  useEffect(() => {
+  function fetchLatestLog() {
     fetch("http://localhost:3010/log/latest")
       .then(response => response.text())
       .then(text => { 
-        console.log(text);
         setState(text);
       }
     )
-  }, [])
+  }
+
+  useEffect(fetchLatestLog, []);
+
+  // const interval = useInterval(fetchLatestLog, latestLogFetchIntervalMs);
 
   return (
     <div className="App">
